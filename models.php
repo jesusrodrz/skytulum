28ed65da713fe839cef5a02c9727f294d442fa8d
Jesus Rodriguez <jesus.rodrz97@gmail.com>
2018-10-22 13:53:34 -0300

    models fig

--- models.php
+++ models.php
@@ -86,47 +86,5 @@ get_header();
 <?php endwhile; wp_reset_query(); ?>
 </section>
 <?php 
-    // if ( have_posts() ) : 
-    //   while ( have_posts() ) : the_post();
-    //   $post_id = get_the_ID();
-      
-    //       $post_meta = get_post_meta( $post->ID,  'modelos_custom_field', true );
-
-    //       echo $post_meta['list'];
-            
-    //   endwhile;
-    // endif;
-    
-    ?>
-
-<!-- <section class="section-models models">
-  <article class="models__item">
-    <h2 class="models__title section__title title-2 bg-square-center t-uppercase">Estudio</h2>
-    <div class="models__description">
-      <p class="models__text">65.8 m2</p>
-      <ul class="models__list">
-        <li class="models__list-item">1 baño</li>
-        <li class="models__list-item">Totalmente Amueblado</li>
-        <li class="models__list-item">Totalmente Equipado</li>
-        <li class="models__list-item">Jacuzzi Privado</li>
-      </ul>
-    </div>
-    <figure class="models__fig"> <img class="models__img" src="<?php get_asset('assets/img/sky-roof.jpg'); ?>" alt=""/><span class="models__sold">Vendido</span></figure>
-  </article>
-  <article class="models__item">
-    <h2 class="models__title section__title title-2 bg-square-center t-uppercase">1 Habitación</h2>
-    <div class="models__description">
-      <p class="models__text">59.0 m2</p>
-      <ul class="models__list">
-        <li class="models__list-item">1 baño</li>
-        <li class="models__list-item">Totalmente Amueblado</li>
-        <li class="models__list-item">Totalmente Equipado</li>
-        <li class="models__list-item">Jacuzzi Privado</li>
-      </ul>
-    </div>
-    <figure class="models__fig"> <img class="models__img" src="<?php get_asset('assets/img/sky-roof.jpg'); ?>" alt=""/><span class="models__sold">Vendido</span></figure>
-  </article>
-</section> -->
-
-<?php 
+ 
 get_footer();
