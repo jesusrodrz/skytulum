d935738d81d13734a0508da8245fa347bb3d12b8
Jesus Rodriguez <jesus.rodrz97@gmail.com>
2018-10-27 21:56:16 -0300

    favicon

--- functions.php
+++ functions.php
@@ -360,7 +360,7 @@ add_action( 'init', 'specs_levels_taxonomy');
 add_theme_support( 'post-thumbnails' );
 
 // REMOVE GENESIS FAVICON
-// remove_action('genesis_meta', 'genesis_load_favicon');
+remove_action('genesis_meta', 'genesis_load_favicon');
 
 /** Adding custom Favicon */
 add_filter( 'genesis_pre_load_favicon', 'custom_favicon' );
--- header.php
+++ header.php
@@ -4,12 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="<?php get_asset('assets/img/favicon.ico')?>"/>
+    <link rel="icon" href="<?php get_asset('assets/img/favicon.ico')?>"/>
     <?php wp_head(); ?>
-    <link rel="icon" href="<?php get_asset('assets/img/favicon.ico')?>"/>
-    <link rel="icon" href="<?php get_asset('assets/img/favicon.ico')?>"/>
-    <!-- GOOGLE MAPS START -->
       
-    <!-- GOOGLE MAPS END -->
     <title>
       <?php 
         if(is_front_page() || is_home()){
