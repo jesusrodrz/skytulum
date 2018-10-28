2a6bcd28667748e2ac7c1ff334149bdf2e3214c0
Jesus Rodriguez <jesus.rodrz97@gmail.com>
2018-10-28 16:14:25 -0300

    word space link

--- 404.php
+++ 404.php
@@ -10,7 +10,7 @@ get_header(); ?>
   <div class="post__text">
     <h1><?php esc_html_e('Oops!','sky-tulum'); ?></h1>
     <h2><?php esc_html_e('Error 404','sky-tulum'); ?></h2>
-    <p><?php esc_html_e('No encontramos la página que buscas, intenta buscar desde el','sky-tulum'); ?><a href="<?php echo home_url(); ?>"><?php esc_html_e('Inicio','sky-tulum'); ?></a> </p>
+    <p><?php esc_html_e('No encontramos la página que buscas, intenta buscar desde el','sky-tulum'); ?> <a href="<?php echo home_url(); ?>"><?php esc_html_e('inicio','sky-tulum'); ?></a>.</p>
   </div>
 </section>
 <?php get_footer(); ?>
